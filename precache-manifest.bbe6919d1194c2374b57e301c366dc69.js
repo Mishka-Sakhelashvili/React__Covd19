f62963c17fdfc7d68639a4be182f01850919e573
Mikheil Sakhelashvili <mishka.sakhelashvili@gmail.com>
2021-03-12 17:17:54 +0400

    Updates

---
+++ precache-manifest.bbe6919d1194c2374b57e301c366dc69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c816107f7c863218a37ab784419da9eb",
+    "url": "/React__Covd19/index.html"
+  },
+  {
+    "revision": "57ac433785522ad6da22",
+    "url": "/React__Covd19/static/css/main.c294a15b.chunk.css"
+  },
+  {
+    "revision": "958b702401aff7833f20",
+    "url": "/React__Covd19/static/js/2.b33c371b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/React__Covd19/static/js/2.b33c371b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ac433785522ad6da22",
+    "url": "/React__Covd19/static/js/main.2604670c.chunk.js"
+  },
+  {
+    "revision": "956929ed7fc1c27d65a2",
+    "url": "/React__Covd19/static/js/runtime-main.e2a3a60b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/React__Covd19/static/media/image.d7265326.png"
+  }
+]);
